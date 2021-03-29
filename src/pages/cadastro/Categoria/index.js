5ce7fe1b923034c1ac850148e21831368656ab65
elias <eliasfaculdade@yahoo.com.br>
2021-03-29 11:28:56 -0300

    alteracao useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+  console.log(nomeDaCategoria);
+
+  // function funcaoHandlerQueOErroPediu() {
+
+  // }
+
   return (
     <PageDefault>
-      <h1>Cadatro de Categoria</h1>
+      <h1>Cadatro de Categoria: {nomeDaCategoria}</h1>
       <form>
+        {/* ex: State */}
         <label>
           Nome:
-          <input type="text" />
+          <input
+            type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
+              console.log('[nomeDaCategoria]', nomeDaCategoria);
+              console.log('[informacao do evento]', infosDoEvento.target.value);
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
+          />
         </label>
         <button>Cadastrar</button>
       </form>
